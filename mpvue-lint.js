ed0e931f26babdfffb221e338de83609b646d5cb
buting2014 <>
2018-07-29 00:44:54 +0800

    mpvue-lint findComponents

--- mpvue-lint.js
+++ mpvue-lint.js
@@ -1,27 +1,27 @@
 const {window, commands, Disposable, ExtensionContext, StatusBarAlignment, StatusBarItem, TextDocument} = require('vscode');
 function activate(context) {
     console.log(context)
-    findAllImport();
+    findAllComponents();
 }
-function findAllImport (){
+function findAllComponents (){
     // 获取当前文本中的所有import
     let editor = window.activeTextEditor;
     if(!editor){
         return;
     }
-    this.getWordCount = function(doc){
+    this.getAllComponents = function(doc){
         let docContent = doc.getText();
-        console.log(docContent);
-        // docContent = docContent.replace(/(< ([^>]+)<)/g, '').replace(/\s+/g, ' ');
-        // docContent = docContent.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-        // if(docContent !== ''){
-        //     wordCount = docContent.split(" ").length;
-        // }
-        // return wordCount;
+        let componentsArray = [];
+        docContent = docContent.match(/components\s*:\s*\{[^\}]+\}/g)[0];
+        docContent = docContent.match(/\{[\s\S]+\}/g)[0];
+        docContent = docContent.slice(1,docContent.length-1).replace(/\s+/g,'');
+        componentsArray = docContent.split(',');
+        return componentsArray;
     }
     let doc = editor.document;
-    if(doc.languageId == "javascript"){
-        this.getWordCount(doc);
+    if(doc.languageId == "vue"){
+        let components = this.getAllComponents(doc);
+        console.log(components);
     }else {
         console.log('no vue');
     }
